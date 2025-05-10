f4827c663f14b0e13321e132447936c1ea30f5ce
EdwinRajesh <edwinrajesh009@gmail.com>
2025-05-10 10:47:30 +0530

    auth done

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,20 @@ import './index.css'
 import { BrowserRouter as Router } from 'react-router';
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
 import { App } from './App';
+import { AuthProvider } from './context/AuthContext';
 
 const client=new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={client}>
+        <AuthProvider>
+  
     <Router>
       <App />
 
     </Router>
+        </AuthProvider>
+
         </QueryClientProvider>
 
   </StrictMode>,
